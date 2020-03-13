cef05307d136ebee135a9a88846f83ce06af1e7a
tonem123 <tonem123@naver.com>
2020-03-13 15:19:02 +0900

    컴포넌트를 이용함

---
+++ src/main/component/Customer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Customer extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>홍길동</h1>
+				<p>960809</p>
+				<p>남자</p>
+				<p>대학생</p>
+			</div>
+		);
+	}
+}
+
+export default Customer;
--- src/main/js/Page1Page.js
+++ src/main/js/Page1Page.js
@@ -1,4 +1,5 @@
 import '../webapp/css/custom.css';
+import Customer from '../component/Customer';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -9,47 +10,11 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 
-const customers = [
-	{
-		'id': 1,
-		'name': '마성호',
-		'birthday': '960809',
-		'gender': '남',
-		'job': '대학생'
-	},
-	{
-		'id': 2,
-		'name': '심승보',
-		'birthday': '960309',
-		'gender': '남',
-		'job': '대학생'
-	},
-	{
-		'id': 3,
-		'name': '김덕연',
-		'birthday': '960508',
-		'gender': '남',
-		'job': '대학생'
-	}
-]
-
 class Page1Page extends React.Component {
 
   render() {
     return(
-    	<div className="page1">
-    		<Table>
-    			<TableHead>
-    				<TableRow>
-    	    			<TableCell>번호</TableCell>
-    	     			<TableCell>이름</TableCell>
-    	     			<TableCell>생년월일</TableCell>
-    	     			<TableCell>성별</TableCell>
-    	     			<TableCell>직업</TableCell>
-    	    		 </TableRow>
-    	    	</TableHead>
-    	     </Table>
-    	</div>
+    	<Customer/>
     );
   }
 }
